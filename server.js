7100199da5861d06a033a43067a4ba535c656f98
rc-rohan <rohan6hawat@gmail.com>
2021-03-05 15:26:10 +0530

    setup of mogoDB and mongoose with successful connection with DB

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+const mongoose = require('mongoose');
 require('dotenv').config({ path: './config.env' });
 
 const app = require('./app');
 
 // console.log(process.env);
+const DB = process.env.DB_URL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => console.log('Database connection successful!'));
 
 //Start server
 const port = process.env.port || 8000;
